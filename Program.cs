d7d5cf23b5ea2993dd16970dccd6ff9f14154904
pasit-GDM025 <pasit.pipat@mail.kmutt.ac.th>
2023-03-28 20:14:58 +0700

    Hokk_pip

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program {
+    static void Main(string[]args){
+        int MaxCo = 100;
+        double[] x, y;
+        x = new double[MaxCo];
+        y = new double[MaxCo];
+
+        int i;
+        for(i=0; i<MaxCo;i++){
+            double X, Y; 
+            X = double.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine());
+            if(X != 0 || Y != 0){
+                x[i] = X;
+                y[i] = Y;
+            }
+            else{
+                break;
+            }
+        }
+        double Kx, Ky;
+        Kx = double.Parse(Console.ReadLine());
+        Ky = double.Parse(Console.ReadLine());
+
+        int u = i-1;
+        bool inside = true;
+        for(i =0; i< u;i++){
+            if (((y[i] > Ky) != (y[u] > Ky)) && (Kx < (x[u] - x[i]) * (Ky - y[i]) / (y[u] - y[i]) + x[i]) || Kx == 0 && Ky == 0) {
+                inside = !inside;
+            }
+            u = i;
+        }
+        if(inside){
+            Console.WriteLine("inside");
+        }
+        else{
+            Console.WriteLine("outside");
+        }
+    }
+}
